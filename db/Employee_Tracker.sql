aa98306206f95519ca641055d414dc7c166c0383
Alisha Panday <panday.alisha90@gmail.com>
2021-02-14 15:21:40 +0800

    created employee db and inserted value into it

--- db/Employee_Tracker.sql
+++ db/Employee_Tracker.sql
@@ -7,19 +7,26 @@ CREATE TABLE Employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR (30)NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
     PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES Role(id),
+    FOREIGN KEY (manager_id) REFERENCES Role(id)
+    
 )
 
 CREATE TABLE Role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30)NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INT,
     PRIMARY KEY (id)
+    FOREIGN KEY (department_id) REFERENCES Department(id)
 
 )
 
 CREATE TABLE Department (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (30)NOT NULL,
     PRIMARY KEY (id)
 )
